415c649fd00a724f1ec1631e3e8142b512233059
Kathy Nguyen <143839810+pluemebe@users.noreply.github.com>
2024-08-26 21:46:35 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  let passenger = "Taco Cat";
+
+  return (
+    <div className="App">
+      <h1>The burrito truck holds {passenger}</h1>
+    </div>
+  );
+}
+
+export default App;
